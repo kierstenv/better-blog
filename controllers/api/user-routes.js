64468b51dfe8eb92f430b4a3a2b33581682ed474
kierstenv <kierstenvicknair22@gmail.com>
2022-06-30 22:03:41 -0500

    create login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -41,8 +41,29 @@ router.post('/', (req, res) => {
   );
 });
 
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that username!' });
+        return;
+      }
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 router.put('/:id', (req, res) => {
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
@@ -52,6 +73,7 @@ router.put('/:id', (req, res) => {
         res.status(404).json({ message: 'No user found with this id!' });
         return;
       }
+      
       res.json(dbUserData);
     })
     .catch(err => {
