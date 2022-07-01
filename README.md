db2b455a49ef71ea94d683a5dfb982ea57db5a11
kierstenv <kierstenvicknair22@gmail.com>
2022-07-01 09:11:37 -0500

    deploy to heroku

---
+++ README.md
@@ -0,0 +1,19 @@
+# better-blogger!
+  
+  ## Description [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg?style=flat-square)](https://opensource.org/licenses/MIT)
+  
+  Better Blogger is a CMS-style blog site where users can create an account, publish posts, and interact with a community of developers!
+  
+  ***
+  
+  ## Usage 
+
+  Try [Better Blogger](https://better-blogger.herokuapp.com/login)!
+  
+  ## License
+
+  Licensed under the MIT license. See [LICENSE.TXT](./LICENSE.TXT) for more information.
+
+  ## Credits 
+[![UT Austin CBC](https://img.shields.io/badge/-UT%20Austin%20CBC-orange?style=flat-square)](https://techbootcamps.utexas.edu/coding//)
+  
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -47,12 +47,15 @@ router.post('/', (req, res) => {
     username: req.body.username,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  );
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
+      res.json(dbUserData);
+    });
+  })
 });
 
 router.post('/login', (req, res) => {
@@ -66,13 +69,23 @@ router.post('/login', (req, res) => {
         res.status(400).json({ message: 'No user with that username!' });
         return;
       }
+
       const validPassword = dbUserData.checkPassword(req.body.password);
+      
       if (!validPassword) {
         res.status(400).json({ message: 'Incorrect password!' });
         return;
       }
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });
+
+      req.session.save(() => {
+
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+  });
 });
 
 router.put('/:id', (req, res) => {
