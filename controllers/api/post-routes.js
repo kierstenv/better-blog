81f56878319672a96d9f9c7b450f3f85e4f863fe
kierstenv <kierstenvicknair22@gmail.com>
2022-07-01 03:27:05 -0500

    integrate comments into api routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
-  console.log('======================');
   Post.findAll({
     attributes: ['id', 'post_url', 'title', 'created_at'],
     order: [['created_at', 'DESC']],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
